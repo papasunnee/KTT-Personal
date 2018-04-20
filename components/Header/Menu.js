8181a0cc4ec9bb7c612974530b940a6cebbf08b5
Adetona Sunday <papasunnee@gmail.com>
2018-04-20 12:08:55 +0100

    dede

--- components/Header/Menu.js
+++ components/Header/Menu.js
@@ -6,21 +6,21 @@ export default (props) => {
         <div className={"turaki-menu-wrapper " + (props.isBottom ? "bottom" : "")}>
             <nav className="turaki-main-nav ">
                 <ul>
-                    {/* <li className="waves-effect waves-turaki scroll"><a href="./index.html#about">About</a></li>
+                    <li className="waves-effect waves-turaki scroll"><a href="./index.html#about">About</a></li>
                     <li className="waves-effect waves-turaki"><a href="./education.html">Education</a></li>
                     <li className="waves-effect waves-turaki"><a href="./profession.html">Profession</a></li>
                     <li className="turaki-home waves-effect scroll"><a href="./index.html">KTT <small>(SAN)</small></a></li>
                     <li className="waves-effect waves-turaki"><a href="./politics.html">Politics</a></li>
                     <li className="waves-effect waves-turaki scroll"><a href="./honours.html">Honours</a></li>
-                    <li className="waves-effect waves-turaki scroll"><a href="#contact">Contact</a></li>  */}
+                    <li className="waves-effect waves-turaki scroll"><a href="#contact">Contact</a></li> 
 
-                    <li className="waves-effect waves-turaki scroll"><a href="/#about">About</a></li>
+                    {/* <li className="waves-effect waves-turaki scroll"><a href="/#about">About</a></li>
                     <li className="waves-effect waves-turaki"><Link prefetch href="/education"><a>Education</a></Link></li>
                     <li className="waves-effect waves-turaki"><Link prefetch href="/profession"><a>Profession</a></Link></li>
                     <li className="turaki-home waves-effect scroll"><Link prefetch href="/"><a href="#home">KTT <small>(SAN)</small></a></Link></li>
                     <li className="waves-effect waves-turaki"><Link prefetch href="/politics"><a>Politics</a></Link></li>
                     <li className="waves-effect waves-turaki"><Link prefetch href="/honours"><a>Honours</a></Link></li>
-                    <li className="waves-effect waves-turaki scroll"><Link prefetch href="#contact"><a>Contact</a></Link></li>
+                    <li className="waves-effect waves-turaki scroll"><Link prefetch href="#contact"><a>Contact</a></Link></li> */}
                 </ul>
             </nav>
         </div>
--- components/Header/MobileMenu.js
+++ components/Header/MobileMenu.js
@@ -7,21 +7,21 @@ export default (props) => {
         <div className="nav-header"><span className="ti-menu mobile-site-menu"></span><a className="mobile-site-title" href="#">Kabiru Tanimu (SAN)</a></div>
         <nav className="turaki-mobile-nav scroll">
           <ul>
-            {/* <li><a className="waves-effect waves-turaki" href="./index.html">KTT <small>(SAN)</small></a></li>
+            <li><a className="waves-effect waves-turaki" href="./index.html">KTT <small>(SAN)</small></a></li>
             <li><a className="waves-effect waves-turaki" href="./index.html#about">About</a></li>
             <li><a className="waves-effect waves-turaki" href="./education.html">Education</a></li>
             <li><a className="waves-effect waves-turaki" href="./profession.html">Profession</a></li>
             <li><a className="waves-effect waves-turaki" href="./politics.html">Politics</a></li>
             <li><a className="waves-effect waves-turaki" href="./honours.html">Honours</a></li>
-            <li><a className="waves-effect waves-turaki" href="#contact">Contact</a></li> */}
+            <li><a className="waves-effect waves-turaki" href="#contact">Contact</a></li>
 
-            <li><a className="waves-effect waves-turaki" href="./index">KTT <small>(SAN)</small></a></li>
+            {/* <li><a className="waves-effect waves-turaki" href="./index">KTT <small>(SAN)</small></a></li>
             <li><a className="waves-effect waves-turaki" href="./index#about">About</a></li>
             <li><a className="waves-effect waves-turaki" href="./education">Education</a></li>
             <li><a className="waves-effect waves-turaki" href="./profession">Profession</a></li>
             <li><a className="waves-effect waves-turaki" href="./politics">Politics</a></li>
             <li><a className="waves-effect waves-turaki" href="./honours">Honours</a></li>
-            <li><a className="waves-effect waves-turaki" href="#contact">Contact</a></li>
+            <li><a className="waves-effect waves-turaki" href="#contact">Contact</a></li> */}
           </ul>
         </nav>
       </div>
